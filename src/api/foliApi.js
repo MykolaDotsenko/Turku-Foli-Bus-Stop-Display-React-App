9c9afc8be514c3d4558d5b1f4a2d52539451816d
Mykola D <130664123+MykolaDotsenko@users.noreply.github.com>
2026-09-19 09:19:36 +0300

    Use Vite environment variables

--- src/api/foliApi.js
+++ src/api/foliApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  process.env.REACT_APP_FOLI_API_URL || "https://data.foli.fi/siri/sm";
+  import.meta.env.VITE_FOLI_API_URL || "https://data.foli.fi/siri/sm";
 
 const client = axios.create({
   timeout: 8000,
