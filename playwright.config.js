a111e8c250fb1da9a212cfe77d4fafcf6ae720d9
Mykola D <130664123+MykolaDotsenko@users.noreply.github.com>
2026-09-20 12:01:36 +0300

    Discover only explicit Playwright e2e specs

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,7 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./e2e",
+  testMatch: "**/*.e2e.js",
   fullyParallel: true,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
